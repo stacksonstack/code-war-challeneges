9828ba4bc8383aff7d4d0b7092f6f37901b40079
stacksonstack <staceycarrillo44@gmail.com>
2021-01-19 18:33:48 -0500

    second problem done

--- Jan19.js
+++ Jan19.js
@@ -16,6 +16,24 @@ function sumTwoSmallestNumbers(numbers) {
       return num1 + num2
     //this is using destructuring - second solution
   }
-  console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77])) // output: 7
 
-  console.log(concise([19, 5, 42, 2, 77])) // output: 7
+//   console.log(sumTwoSmallestNumbers([19, 5, 42, 2, 77])) // output: 7
+//   console.log(concise([19, 5, 42, 2, 77])) // output: 7
+
+
+//   Given two integers a and b, which can be positive or negative, find the sum of 
+//   all the integers between including them too and return it. If the two numbers are 
+//   equal return a or b.
+
+
+function getSum( a,b ) {
+    var[ lowest, highest] = [ Math.min(a,b), Math.max(a,b)];
+    var sumOfRange = 0
+    while(lowest <= highest){
+        sumOfRange += lowest
+        lowest++
+    }
+    return sumOfRange
+}
+
+// console.log(getSum(-1, 2)) // output = 2
