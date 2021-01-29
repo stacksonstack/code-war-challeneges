ef189e73c6f70f35b48710c8bac25bb5d8816f6c
stacksonstack <staceycarrillo44@gmail.com>
2021-01-28 20:01:25 -0500

    second challege done

--- Jan28.js
+++ Jan28.js
@@ -32,4 +32,18 @@ function incrementString(string) {
 
 
 
+// Write an algorithm that takes an array and moves all of the zeros to the end, preserving the order of the other elements.
+
+function moveZeross(array){
+    let zeros = array.filter((zero)=> zero === 0)
+    let nonZeros = array.filter((notZero) => notZero !== 0)
+    return [...nonZeros, ...zeros]
+ }
+ 
+ const moveZeros =(array)=> [...array.filter((notZero) => notZero !== 0), ...array.filter((zero)=> zero === 0)]
+    
+     
+  
+ 
+ console.log(moveZeros([false,1,0,1,2,0,1,3,"a"])) // returns[false,1,1,2,1,3,"a",0,0]
 
