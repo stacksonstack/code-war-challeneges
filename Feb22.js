1eb9e8c0512442fc748a7bb6273753ef03f7299a
stacksonstack <staceycarrillo44@gmail.com>
2021-02-22 18:15:37 -0500

    third challenege done

--- Feb22.js
+++ Feb22.js
@@ -56,6 +56,26 @@ multiplicationTable = function(size) {
 
 
 
-console.log(multiplicationTable(3)) //, [[1,2,3], [2,4,6], [3,6,9]]);
+// console.log(multiplicationTable(3)) //, [[1,2,3], [2,4,6], [3,6,9]]);
 
-console.log(multiplicationTable(4))
+// console.log(multiplicationTable(4))
+
+
+// A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence 
+// "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+
+// Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string){
+  let arr = string.split("")
+  let alphabet = "abcdefghijklmnopqrstuvwxyz".split("")
+  let pangramCheck =  alphabet.map((letter) => arr.includes(letter))
+  return !pangramCheck.includes(false)
+}
+
+
+
+var string = "The quick brown fox jumps over the lazy dog."
+console.log(isPangram(string)) //, true)
+var string2 = "This is not a pangram."
+console.log(isPangram(string2)) //, false)
